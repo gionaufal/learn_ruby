b20ce6d0c0613eabb7e39f0628c9f645584f89a4
gionaufal <g10v45+github@gmail.com>
2016-07-21 09:06:32 -0300

    add simon_says exercise

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,26 @@
-#write your code here
+def echo(string)
+  string
+end
+
+def shout(string)
+  string.upcase
+end
+
+def repeat(string, times = 2)
+  result = (string + ' ')* times
+  result[0...-1]
+end
+
+def start_of_word(string, num)
+  string[0...num]
+end
+
+def first_word(string)
+  array = string.split(' ')
+  array[0].to_s
+end
+
+def titleize(string)
+  stop_words = %w{a an and the or for of nor over}
+  string.split.each_with_index.map{|word, index| stop_words.include?(word) && index > 0 ? word : word.capitalize }.join(" ")
+end
