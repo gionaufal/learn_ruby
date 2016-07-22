15f5398cbb61c0e5969b3b57a58ee22aa4c33dac
gionaufal <g10v45+github@gmail.com>
2016-07-22 17:05:20 +0000

    add pig_latin.rb

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,8 @@
-#write your code here
+def translate (word)
+    if %w(a e i o u).include?(word[0])
+        word+'ay'
+    else
+        word_cons = word[1...word.length]+word[0]+'ay'
+        word_cons
+    end
+end
