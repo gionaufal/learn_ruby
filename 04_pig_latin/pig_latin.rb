ad73d130dc8fd1bf0ad4440070fbe117c7f08d14
gionaufal <g10v45+github@gmail.com>
2016-07-26 08:42:19 -0300

    update pig_latin.rb - needs improvement

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,8 +1,11 @@
 def translate (word)
     if %w(a e i o u).include?(word[0])
         word+'ay'
-    else
+    elsif !(%w(a e i o u).include?(word[0])) && %w(a e i o u).include?(word[1])
         word_cons = word[1...word.length]+word[0]+'ay'
+        word_cons
+    elsif (!(%w(a e i o u).include?(word[0])) && !(%w(a e i o u).include?(word[1])))
+        word_cons = word[2...word.length]+word[0...1]+'ay'
         word_cons
     end
 end
