25f8982145e02eed2938b75d3121ec963ca792b3
gionaufal <g10v45+github@gmail.com>
2016-07-29 08:38:30 -0300

    update pig_latin.rb with a correct solution

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,11 +1,21 @@
-def translate (word)
-    if %w(a e i o u).include?(word[0])
-        word+'ay'
-    elsif !(%w(a e i o u).include?(word[0])) && %w(a e i o u).include?(word[1])
-        word_cons = word[1...word.length]+word[0]+'ay'
-        word_cons
-    elsif (!(%w(a e i o u).include?(word[0])) && !(%w(a e i o u).include?(word[1])))
-        word_cons = word[2...word.length]+word[0...1]+'ay'
-        word_cons
+def translate(string)
+
+    words = string.split
+    translated = []
+
+    for string in words
+      qu_last_index = string.rindex(/[qu]/)
+      first_vowel_index = string.index(/[aeiou]/)
+
+      if first_vowel_index == 0 || first_vowel_index.nil?
+        result = string + "ay"
+      elsif qu_last_index
+        result = string[(qu_last_index + 1)..string.length] + string[0..qu_last_index] + "ay"
+      else
+        result = string[first_vowel_index..string.length] + string[0..(first_vowel_index-1)] + "ay"
+      end
+      translated << result
     end
+
+    translated.join(' ')
 end
