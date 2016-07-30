5dba7b240081f13103a5bcf2b4649c61dafa7e97
gionaufal <g10v45+github@gmail.com>
2016-07-30 12:58:49 -0300

    add book.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
-# write your code here
+
+  attr_reader :title
+
+  def title=(str)
+
+    words = str.split(' ')
+    new_title = []
+
+    words.each_with_index do |word, index|
+      if word.length > 2 || index = 0 || word = 'i'
+        new_title << word.capitalize
+      else
+        new_title << word
+      end
+    end
+
+    @title = new_title.join(' ')
+
+  end
+
+  def title
+    @title
+  end
+
 end
